96baf916efb2f53f2c17c50ae4ce78b390586e04
Rolando <rolandoquintanaa@gmail.com>
2025-03-03 17:07:45 -0500

    Added EmailJS initialization and fixed contact form integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import emailjs from "@emailjs/browser"; // Added EmailJS import
+
+// Initialize EmailJS with your Public Key
+emailjs.init("UtCZ-8d4_Ht9XhNxI"); // Using your Public Key from the ContactForm code
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
